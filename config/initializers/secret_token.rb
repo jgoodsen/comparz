389108d469f5e450352622705493803f2db21322
@jgoodsen <jgoodsen@radsoft.com>
2011-04-10 19:33:28 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComparzCom::Application.config.secret_token = '6198342058df5c50c7121cc72fc2bc1f5f721febe6355f5f1b825c26d622bc83b5cb80ba61a56497b502ec16f2158ecafb58378ea9a6bf0a51f630512adeba09'
